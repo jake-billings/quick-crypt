cbb54d79060c8fc29671fcf5583b8406e79bcc9c
jakebillings <jake@jakebillings.com>
2017-06-29 10:26:03 -0600

    Add firebase keys and module imports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRWfrXdylvI_YnndEKpFu86X-MaO17frQ",
+    authDomain: "quick-crypt.firebaseapp.com",
+    databaseURL: "https://quick-crypt.firebaseio.com",
+    projectId: "quick-crypt",
+    storageBucket: "",
+    messagingSenderId: "982046979370"
+  }
 };
